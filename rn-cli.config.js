0e9ce10cb000cfa4864794b0572f3ea219c13481
Stanley Yoshinori Takamatsu <stanleytakamatsu@gmail.com>
2019-04-24 18:19:39 -0300

    Update configurations

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,13 @@
+const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer');
   },
   getSourceExts() {
     return ['ts', 'tsx'];
+  },
+  resolver: {
+    blacklistRE: blacklist([/\/www\/src\/.*/])
   }
 };
